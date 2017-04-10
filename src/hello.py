3c86443c356c77c6a4c64df4e02c1cb02a12ccfd
AutuanLiu <AutuanLiu@gmail.com>
2017-04-10 23:55:02 +0800

    create a new repository.

---
+++ src/hello.py
@@ -0,0 +1,19 @@
+# the first python script
+print('hello, world!')
+print("hello, world!")
+# '' and "" 是一样的
+#Enter a code
+print(45678 + 0x12fd2)
+print("Learn Python in imooc")
+print(100 < 99)
+print(0xff == 255)
+# 使用 , 进行连接
+print('The quick brown fox', 'jumps over', 'the lazy dog')
+print(u'世界，你好！')
+print(u'''静夜思
+
+床前明月光，
+疑是地上霜。
+举头望明月，
+低头思故乡。''')
+print (11.0/4)
