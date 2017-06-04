32355cf02f6bfdfe531167614fda4f131e30cd6c
AutuanLiu <AutuanLiu@gmail.com>
2017-06-04 13:46:44 +0800

    pandas added

--- src/pandast.py
+++ src/pandast.py
@@ -7,5 +7,17 @@ Created on Sun Jun  4 12:56:53 2017
 
 import pandas as pd
 import numpy as np
+
 a = pd.Series([9, 8, 7, 6])
 d = pd.DataFrame(np.arange(10).reshape(2, 5))
+dt = {"one": pd.Series([1, 2, 3], index = ['a', 'b', 'c']),
+      "two": pd.Series([9, 8, 7, 6], index = ['a', 'b', 'c', 'd'])}
+d1 = pd.DataFrame(dt)
+print(d1)
+print(d)
+print(d1)
+
+# from list dict create dataframe
+d2 = {"one": [1, 2, 3, 4], "two": [9, 8, 7, 6]}
+d3 = pd.DataFrame(d2, index = ['a', 'b', 'c', 'd'])
+print(d3)
